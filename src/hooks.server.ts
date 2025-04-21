dc36b4919b6db4763ff3610a2358d28a52d30e6c
lcs86 <lcs86@atomrigs.io>
2025-04-21 14:05:25 +0900

    remove unused hooks.server.ts file and its associated imports

--- src/hooks.server.ts
+++
@@ -1,6 +0,0 @@
-import { auth } from './lib/auth';
-import { svelteKitHandler } from './svelte-kit-handler';
-
-export async function handle({ event, resolve }) {
-	return svelteKitHandler({ event, resolve, auth });
-}
